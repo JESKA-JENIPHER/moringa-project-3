f8e4e5d9c28b83e7fc285c7485818e7a6c952ea0
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-05-14 19:06:58 +0300

    Add an array of answers

--- assets/index.css/style.js
+++ assets/index.css/style.js
@@ -2,19 +2,19 @@
 var studentDetails = document.querySelector("#studentDetails");
 var headerText = document.querySelector("#headerText");
 var detailsEntered = document.querySelector("#detailsEntered");
-let quiz = document.querySelector("#quiz")
-let answerA = document.querySelector("#answerA")
-let answerB = document.querySelector("#answerB")
-let answerC = document.querySelector("#answerC")
-let answerD = document.querySelector("#answerD")
+let quiz = document.querySelector("#quiz");
+let answerA = document.querySelector("#answerA");
+let answerB = document.querySelector("#answerB");
+let answerC = document.querySelector("#answerC");
+let answerD = document.querySelector("#answerD");
 let qnChoice = "";
 let score = 0;
 let iterationNumber = 0;
-let question = document.querySelector("#question")
-let qn1button = document.querySelector("#qn1button")
-let qn2button = document.querySelector("#qn2button")
-let qn3button = document.querySelector("#qn3button")
-let qn4button = document.querySelector("#qn4button")
+let question = document.querySelector("#question");
+let qn1button = document.querySelector("#qn1button");
+let qn2button = document.querySelector("#qn2button");
+let qn3button = document.querySelector("#qn3button");
+let qn4button = document.querySelector("#qn4button");
 
 
 let questionList = [
@@ -24,6 +24,10 @@ let questionList = [
   "Qn4.Who is  the father of computing?"
 ]
 
+let aAnswerList = ["Javascript", "Cascading sport", "HTML", "Alex Graham"]
+let bAnswerList = ["Jstick", "Cascading Style Sheet", "Hatmall", "John Babbage"]
+let cAnswerList = ["Javas", "CSports", "Hypertext mark Up language", "Mike tyson"]
+let dAnswerList = ["None", "Cascading Styles", "Hypertext", "Charles Babbage"]
 
 quiz.style.display = "none";
 qn1button.style.display = "none";
@@ -57,7 +61,7 @@ function bFunction() {
   answerC.style.color = "white"
   answerD.style.color = "white"
 
-   qnChoice = "b";
+  qnChoice = "b";
 }
 
 function cFunction() {
@@ -76,9 +80,9 @@ function dFunction() {
   answerD.style.color = "purple"
 
   qnChoice = "d";
-}
+};
 
-submitQn1() {
+function submitQn1() {
   if (qnChoice == "a") {
     score += 1;
   }
@@ -86,9 +90,9 @@ submitQn1() {
   question.innerHTML = questionList[iterationNumber];
   qn1button.style.display = "none";
   qn2button.style.display = "block";
-}
+};
 
-submitQn2() {
+function submitQn2() {
   if (qnChoice == "b") {
     score += 1;
   }
@@ -96,9 +100,9 @@ submitQn2() {
   question.innerHTML = questionList[iterationNumber];
   qn2button.style.display = "none";
   qn3button.style.display = "block";
-}
+};
 
-submitQn3() {
+function submitQn3() {
   if (qnChoice == "c") {
     score += 1;
   }
@@ -106,17 +110,17 @@ submitQn3() {
   question.innerHTML = questionList[iterationNumber];
   qn3button.style.display = "none";
   qn4button.style.display = "block";
-}
+};
 
-submitQn4() {
+function submitQn4() {
   if (qnChoice == "d") {
     score += 1;
   }
   iterationNumber += 1;
-  question.innerHTML = "Your score is" + score;
+  headerText.innerHTML = "Your score is" + score;
   quiz.style.display = "none";
-  
-}
+
+};
 
 
 
